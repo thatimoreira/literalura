d933bcbc5fa15a0351317c130e5e15b5e24c3148
thatimoreira <thatimoreira1@gmail.com>
2024-07-23 10:57:21 -0300

    Cria classe GutendexResponse para mapear a estrutura da resposta JSON da API

--- src/main/java/br/com/alura/literalura/model/GutendexResponse.java
+++ src/main/java/br/com/alura/literalura/model/GutendexResponse.java
@@ -1,2 +1,27 @@
-package br.com.alura.literalura.model;public class Gutendexresponse {
+package br.com.alura.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GutendexResponse {
+    private int count;
+    private List<Livro> results;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public List<Livro> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Livro> results) {
+        this.results = results;
+    }
 }
