edf52364296aa50d564b661ffd5d53f669517c82
thatianamoreira <thatianamoreira@id.uff.br>
2024-07-24 03:30:51 -0300

    Divide responsabilidades da LiteraluraApplication em MenuService, movendo métodos de inserção e consulta de livros para MenuService

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,26 @@
 package br.com.alura.literalura;
 
+import br.com.alura.literalura.service.GutendexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+
+	private final MenuService menuService;
+
+	public LiteraluraApplication(MenuService menuService) {
+		this.menuService = menuService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		menuService.exibirMenu();
+	}
 }
