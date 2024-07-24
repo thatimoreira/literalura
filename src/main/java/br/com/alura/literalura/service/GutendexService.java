b3bedff7933fc79f1bcf0b46e78051597dc088bf
thatianamoreira <thatianamoreira@id.uff.br>
2024-07-24 03:33:24 -0300

    Implementa método de inserção de livros no GutendexService - Adiciona lógica para inserir novos livros - Integra com GutendexClient para armazenar livros localmente

--- src/main/java/br/com/alura/literalura/service/GutendexService.java
+++ src/main/java/br/com/alura/literalura/service/GutendexService.java
@@ -3,25 +3,39 @@ package br.com.alura.literalura.service;
 
 import br.com.alura.literalura.client.GutendexClient;
 import br.com.alura.literalura.model.GutendexResponse;
+import br.com.alura.literalura.model.Livro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class GutendexService {
+
+    private final AtomicInteger idCounter = new AtomicInteger(1);
 
     @Autowired
     private GutendexClient gutendexClient;
 
     public GutendexResponse buscarLivros(String query) {
         try {
-            GutendexResponse response = gutendexClient.buscarLivros(query);
-            return response;
+            return gutendexClient.buscarLivros(query);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
 
             return null;
         }
+    }
+
+    public boolean inserirNovoLivro(String titulo, List<Livro.Autor> autores) {
+        Livro novoLivro = new Livro();
+        novoLivro.setId(idCounter.getAndIncrement());
+        novoLivro.setTitulo(titulo);
+        novoLivro.setAutores(autores);
+
+        return gutendexClient.inserirLivro(novoLivro);
     }
 }
