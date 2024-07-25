a3ecf555085ac73a82d64f2ba6197916b9b12b6f
thatianamoreira <thatianamoreira@id.uff.br>
2024-07-25 04:29:02 -0300

    Atualiza LivroController para melhor gerenciamento de requisições
    - Ajusta endpoints para consultas e manipulações de dados de livros
    - Melhora o manuseio de erros e respostas HTTP
    - Adiciona novos métodos para suporte a outras funcionalidades da aplicação

--- src/main/java/br/com/alura/literalura/controller/LivroController.java
+++ src/main/java/br/com/alura/literalura/controller/LivroController.java
@@ -1,11 +1,14 @@
 package br.com.alura.literalura.controller;
 
 import br.com.alura.literalura.model.GutendexResponse;
+import br.com.alura.literalura.model.Livro;
 import br.com.alura.literalura.service.GutendexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LivroController {
@@ -21,5 +24,15 @@ public class LivroController {
             return response.toString();
         }
         return "Nenhum dado encontrado!";
+    }
+
+    @GetMapping("/api/livros/todos")
+    public List<Livro> listarTodosLivros() {
+        return gutendexService.listarTodosLivrosComAutores();
+    }
+
+    @GetMapping("/api/livros/idioma")
+    public List<Livro> listarLivrosPorIdioma(@RequestParam String idioma) {
+        return gutendexService.listarLivrosPorIdioma(idioma);
     }
 }
