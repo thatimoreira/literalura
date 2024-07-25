2d40617d63dae7a917d1fc72317052ddb60b9840
thatianamoreira <thatianamoreira@id.uff.br>
2024-07-25 04:25:19 -0300

    Adiciona  para operações CRUD no banco de dados para autores

--- src/main/java/br/com/alura/literalura/repository/AutorRepository.java
+++ src/main/java/br/com/alura/literalura/repository/AutorRepository.java
@@ -1,2 +1,9 @@
-package br.com.alura.literalura.repository;public class AutorRepository {
+package br.com.alura.literalura.repository;
+
+import br.com.alura.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Integer> {
 }
