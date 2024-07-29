b3c8b34b62ceeee6e86370e04fc8bbb7c3d3c7ee
thatianamoreira <thatianamoreira@id.uff.br>
2024-07-29 14:31:49 -0300

    Adiciona AppRunner.java para testes iniciais

--- src/main/java/br/com/alura/literalura/AppRunner.java
+++ src/main/java/br/com/alura/literalura/AppRunner.java
@@ -1,2 +1,20 @@
-package br.com.alura.literalura;public class AppRunner {
+package br.com.alura.literalura;
+
+import br.com.alura.literalura.model.GutendexResponse;
+import br.com.alura.literalura.service.GutendexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+
+    @Autowired
+    private GutendexService gutendexService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        GutendexResponse response = gutendexService.buscarLivros("tolstoy", null);
+        System.out.println(response);
+    }
 }
