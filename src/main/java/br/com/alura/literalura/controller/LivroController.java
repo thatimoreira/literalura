8e026a124322560ac5f66bcfff0c71f3a9aaa0e3
thatianamoreira <thatianamoreira@id.uff.br>
2024-07-29 14:05:21 -0300

    Atualiza LivroController: - Altera endpoints de '/api/livros' para '/books' e '/books/' - Adiciona suporte para pesquisa por título e idioma com parâmetros opcionais 'search' e 'languages' Motivo: Padronizar os endpoints da API e oferecer maior flexibilidade nas consultas de livros

--- src/main/java/br/com/alura/literalura/controller/LivroController.java
+++ src/main/java/br/com/alura/literalura/controller/LivroController.java
@@ -1,38 +1,21 @@
 package br.com.alura.literalura.controller;
 
 import br.com.alura.literalura.model.GutendexResponse;
-import br.com.alura.literalura.model.Livro;
 import br.com.alura.literalura.service.GutendexService;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class LivroController {
-
     @Autowired
     private GutendexService gutendexService;
 
-    @GetMapping("/api/livros")
-    public String buscarLivros(@RequestParam String query) {
-        GutendexResponse response = gutendexService.buscarLivros(query);
-
-        if (response != null){
-            return response.toString();
-        }
-        return "Nenhum dado encontrado!";
-    }
-
-    @GetMapping("/api/livros/todos")
-    public List<Livro> listarTodosLivros() {
-        return gutendexService.listarTodosLivrosComAutores();
-    }
-
-    @GetMapping("/api/livros/idioma")
-    public List<Livro> listarLivrosPorIdioma(@RequestParam String idioma) {
-        return gutendexService.listarLivrosPorIdioma(idioma);
+    @GetMapping(value = {"/books", "/books/"})
+    public GutendexResponse getLivros(@RequestParam(name = "search", required = false) String searchQuery,
+                                      @RequestParam(name = "languages", required = false) String language) {
+        return gutendexService.buscarLivros(searchQuery, language);
     }
 }
